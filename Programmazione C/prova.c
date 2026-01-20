ca5576f08d945616470fb1b8cd88aaf319b22c94
Camila Rhoads <camila93rhoads@gmail.com>
2026-01-20 17:28:59 +0100

    "Aggiornamento prova esame 20250617"

--- Programmazione C/prova.c
+++ Programmazione C/prova.c
@@ -1,8 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h
+
+#define MAX 19+1
+
+//Funzioni prototipo 
+
+
+
+void funzioneListaPari(FILE *fInput){
+
+    int numero;
+    printf("Numeri pari presenti nel file di input:\n");
+    while(fscanf(fInput, "%d", &numero) == 1){
+        if(numero % 2 == 0){
+            printf("%d\n", numero);
+        }
+    }
+}
+
+
 
 int main(){
 
-    printf("Ciao Repository Aggiornato 19 Gennaio!\n");
+
+//1-Inserire il nome del file di input
+
+char nomeFileInput[MAX];
+
+printf("Inserisci il nome del file di input:");
+scanf("%s",nomeFileInput);
+
+//2-Apertura del file di input in modalita' lettura
+FILE *fInput = fopen(nomeFileInput, "r");
+if(fInput == NULL){
+    printf("Errore nell'apertura del file di input\n");
+    return 1;
+}
+
+//1a-Funzione lista contententi i numeri pari presenti nel file di input
+
+funzioneListaPari(fInput);
+
+//Chiusura del file di input
+fclose(fInput);
+
+
+
 
     return 0;
 }
