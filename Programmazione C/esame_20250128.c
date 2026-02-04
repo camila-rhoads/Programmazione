2da43ded7570c7be8a53e87097c030cc945e401d
Camila Rhoads <camila93rhoads@gmail.com>
2026-02-04 13:12:34 +0100

    "Inserimento del nome del file"

---
+++ Programmazione C/esame_20250128.c
@@ -0,0 +1,28 @@
+ #include<stdio.h>
+
+ #define MAX 19+1
+
+//in questo eserczio si usano gli array
+
+
+
+ //funzioni Prototipo
+
+
+
+ //funzioni main
+
+
+int main(){
+
+    char nomeFileInput[MAX];
+
+    printf("Inserisci il nome del file da analizzare\n");
+
+    scanf("%s",nomeFileInput);
+
+
+
+
+    return 0;
+}
